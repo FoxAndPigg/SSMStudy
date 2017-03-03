9379d976c160c03f0d24453862d69fea3167aaa5
guozhong <guozhong@qeeniao.com>
2017-03-03 10:42:49 +0800

    ssm init http://www.jianshu.com/p/5124eef40bf0 第一章

--- src/main/java/org/gosky/ssmstudy/controller/ViewController.java
+++ src/main/java/org/gosky/ssmstudy/controller/ViewController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ViewController {
 
-    @RequestMapping("/view")
+    @RequestMapping("view")
     public String view() {
         System.out.println("哈哈哈哈");
         return "index";
